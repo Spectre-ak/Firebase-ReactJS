d4726a23eccedb95d92483a8afc61676644d5d6d
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2020-12-31 21:16:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ import FixedChatArea from './components/FixedChatArea.js';
 import CreateUserName from'./components/CreateUserName.js';
 
 var firebaseConfig = {
-      apiKey: "AIzaSyBCiec200q3DXiXtE54t4PuNFThQnbXCl0",
-      authDomain: "ne-7664f.firebaseapp.com",
-      databaseURL: "https://ne-7664f.firebaseio.com",
-      projectId: "ne-7664f",
-      storageBucket: "ne-7664f.appspot.com",
-      messagingSenderId: "578526438847",
-      appId: "1:578526438847:android:a431b66f2b2e98a9f32d0a",
-     // measurementId: "G-MEASUREMENT_ID",
-  };
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PROJECT_ID.firebaseio.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID",
+};
 firebase.initializeApp(firebaseConfig);
 function CreateTags(){
 	return(
